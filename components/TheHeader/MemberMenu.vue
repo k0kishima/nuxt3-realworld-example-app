08ef60c74978299637ffc3563f3659ee40fc0868
Johndoe <56298669+togane@users.noreply.github.com>
2023-09-19 21:32:49 +0900

    feat: Implement a menu for members

---
+++ components/TheHeader/MemberMenu.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import { authStore } from '~/stores/auth';
+const auth = authStore();
+</script>
+<template>
+  <ul>
+    <li><NuxtLink href="/">Home</NuxtLink></li>
+    <li><a>New Article</a></li>
+    <li><a>Settings</a></li>
+    <li>
+      <a>{{ auth.currentUser?.username }}</a>
+    </li>
+  </ul>
+</template>
+
+<style scoped>
+ul {
+  padding-left: 0;
+  margin-bottom: 0;
+  list-style: none;
+}
+
+li {
+  float: left;
+}
+
+li + li {
+  margin-left: 1rem;
+}
+
+a {
+  color: rgba(0, 0, 0, 0.3);
+  display: block;
+  padding-top: 0.425rem;
+  padding-bottom: 0.425rem;
+}
+</style>
