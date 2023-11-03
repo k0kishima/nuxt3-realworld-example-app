aba0646acb6409bb36df6b4e5554bcece9cf9bd7
Johndoe <56298669+togane@users.noreply.github.com>
2023-11-03 11:15:56 +0900

    feat: Implement mypage

---
+++ pages/@[userName]/index.vue
@@ -0,0 +1,103 @@
+<script setup lang="ts">
+import { useRoute, ref, useFetch, computed } from '#imports';
+import { authStore } from '~/stores/auth';
+import { API_BASE_URL } from '~/constants';
+import { paths } from '~/openapi.gen';
+
+const route = useRoute();
+const userName = route.params.userName;
+
+const auth = authStore();
+const user = auth.currentUser;
+
+if (user == null) {
+  throw new Error('User is not logged in');
+}
+if (typeof userName !== 'string') {
+  throw new TypeError("userName can't be a string");
+}
+
+const menuTabs = [
+  { label: 'My Article', href: `@${userName}` },
+  { label: 'Favorited Articles', href: `@${userName}?favorited=true` },
+];
+const currentTab = ref(
+  route.query.favorited ? 'Favorited Articles' : 'My Article'
+);
+
+const handleActiveTabChange = (newActiveTab: string) => {
+  currentTab.value = newActiveTab;
+};
+
+type GetArticlesResponse =
+  paths['/articles']['get']['responses']['200']['content']['application/json'];
+
+const apiUrl = computed(() => {
+  const url = new URL(`${API_BASE_URL}/articles`);
+  if (currentTab.value === 'Favorited Articles') {
+    url.searchParams.append('favorited', userName);
+  } else {
+    url.searchParams.append('author', userName);
+  }
+  return url.toString();
+});
+
+const { data, pending } = useFetch<GetArticlesResponse>(apiUrl, {
+  method: 'GET',
+  headers: {
+    'Content-Type': 'application/json',
+    Authorization: `Token ${encodeURIComponent(user.token)}`,
+  },
+});
+</script>
+
+<template>
+  <div>
+    <Head>
+      <!-- TODO: display dynamic and use the constant -->
+      <title>{{ userName }} — Conduit</title>
+    </Head>
+
+    <UserJumbotron :user="user" />
+
+    <TheContainer>
+      <TabbedMenu
+        :tabs="menuTabs"
+        :active-tab="currentTab"
+        @update:active-tab="handleActiveTabChange"
+      />
+      <div>
+        <p v-if="pending">Loading articles...</p>
+        <div v-else>
+          <p v-if="data && data.articles && data.articles.length === 0">
+            No articles are here... yet.
+          </p>
+          <div v-else-if="data && data.articles">
+            <ArticleList :articles="data.articles" />
+          </div>
+        </div>
+      </div>
+    </TheContainer>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  margin: 0 auto;
+  width: 60%;
+}
+
+h1 {
+  font-size: 2.5rem;
+  text-align: center;
+}
+
+p {
+  text-align: center;
+  margin-bottom: 1.5rem;
+  a {
+    color: #5cb85c;
+    text-decoration: none;
+  }
+}
+</style>
