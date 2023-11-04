9e547cd64accd119b501485fd3cded794a8eb3b3
Johndoe <56298669+togane@users.noreply.github.com>
2023-11-04 10:44:16 +0900

    feat: Add date utils

---
+++ utils/dateUtils.ts
@@ -0,0 +1,8 @@
+export const formatLongDate = (dateString: string): string => {
+  const date = new Date(dateString);
+  return date.toLocaleDateString('en-US', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  });
+};
