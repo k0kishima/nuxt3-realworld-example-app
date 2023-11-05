860ab32f91bdff5c70f974bc1eadbcb266f8be1d
k0kishima <56298669+k0kishima@users.noreply.github.com>
2023-11-05 12:59:28 +0900

    style: Fix app jumbotron style

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,10 @@ module.exports = {
       colors: {
         'custom-green': '#5CB85C',
       },
+      backgroundColor: (theme) => ({
+        ...theme('colors'),
+        'custom-green': '#5CB85C',
+      }),
       fontFamily: {
         titillium: ['Titillium Web', 'sans-serif'],
       },
