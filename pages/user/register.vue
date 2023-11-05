898620ae99d9c2db0489f26dc186c4b4f38cea9e
k0kishima <56298669+k0kishima@users.noreply.github.com>
2023-11-05 18:11:25 +0900

    style: Fix styles in sign up form

--- pages/user/register.vue
+++ pages/user/register.vue
@@ -1,39 +1,26 @@
+<script setup>
+import { APP_NAME } from '~/constants';
+</script>
+
 <template>
   <div>
     <Head>
-      <!-- TODO: display dynamic and use the constant -->
-      <title>Sign up — Conduit</title>
+      <title>Sign up — {{ APP_NAME }}</title>
     </Head>
 
-    <div class="container">
-      <TheContainer>
-        <h1>Sign up</h1>
-        <p>
-          <NuxtLink href="/user/login">Have an account?</NuxtLink>
+    <TheContainer>
+      <div class="w-full md:w-1/2 md:mx-auto">
+        <h1 class="text-center text-4xl font-medium mb-2">Sign Up</h1>
+        <p class="text-center mb-6">
+          <NuxtLink
+            href="/user/login"
+            class="text-custom-green no-underline hover:text-green-600 hover:underline"
+          >
+            Have an account?
+          </NuxtLink>
         </p>
         <SignupForm />
-      </TheContainer>
-    </div>
+      </div>
+    </TheContainer>
   </div>
 </template>
-
-<style scoped>
-.container {
-  margin: 0 auto;
-  width: 60%;
-}
-
-h1 {
-  font-size: 2.5rem;
-  text-align: center;
-}
-
-p {
-  text-align: center;
-  margin-bottom: 1.5rem;
-  a {
-    color: #5cb85c;
-    text-decoration: none;
-  }
-}
-</style>
