6112967090459a4d4d18625bce963dba23a98032
k0kishima <56298669+k0kishima@users.noreply.github.com>
2024-08-16 18:25:18 +0900

    Add an env

--- constants/index.ts
+++ constants/index.ts
@@ -1,2 +1,3 @@
 export const APP_NAME = 'Conduit';
-export const API_BASE_URL = 'https://api.realworld.io/api';
+export const API_BASE_URL =
+  process.env.API_BASE_URL || 'https://api.realworld.io/api';
